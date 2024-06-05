0f26811ceadfedf9cf882a6f21a2a56105555458
akarshvprakash <akarshvprakash@gmail.com>
2024-06-05 17:53:31 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const encoder = new TextEncoder();
 
 const user = {
   id: 1054,
-  email: "akarshvprakash@gmail.com",
+  email: "rahul1rama@gmail.com",
   name: "Akarsh"
 }
 const challengeBuffer = encoder.encode("34914012789326781858713765455437");
