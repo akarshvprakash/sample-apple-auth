b253c8a3a9908214cf417f94e062631a7c29ad68
you@example.com <you@example.com>
2024-06-07 00:09:12 +0530

    sdf

--- pages/api/generate-authentication-options.ts
+++ pages/api/generate-authentication-options.ts
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const options = generateAuthenticationOptions({
     allowCredentials: [{
-      id: user.id,
+      id: user.credentialID,
       type: 'public-key',
     }],
   });
