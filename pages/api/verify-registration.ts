737589a3796eb1d8312af5563280e423779ee12f
you@example.com <you@example.com>
2024-06-07 00:19:31 +0530

    sdf

--- pages/api/verify-registration.ts
+++ pages/api/verify-registration.ts
@@ -5,28 +5,33 @@ import pool from '../../lib/db';
 import { getCookie } from 'cookies-next'; // Using cookies-next for example
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { body } = req;
-  console.log("username", body);
+  const { username, id, rawId, response, type, clientExtensionResults } = req.body;
+
   // Retrieve the stored challenge from the cookie
   const storedChallenge = getCookie('registrationChallenge', { req, res });
 
-  // const verification = await verifyRegistrationResponse({
-  //   response: body,
-  //   expectedChallenge: storedChallenge as string,
-  //   expectedOrigin: 'https://sample-apple-auth.vercel.app',
-  //   expectedRPID: 'sample-apple-auth.vercel.app',
-  // });
+  const verification = await verifyRegistrationResponse({
+    response: {
+      id,
+      rawId,
+      response,
+      type,
+      clientExtensionResults,
+    },
+    expectedChallenge: storedChallenge as string,
+    expectedOrigin: 'https://sample-apple-auth.vercel.app',
+    expectedRPID: 'sample-apple-auth.vercel.app',
+  });
 
-  // console.log("verification", verification);
+  console.log("verification", verification);
 
-  // if (verification.verified && verification.registrationInfo) {
-  //   const { username } = req.body;
-  //   const { credentialPublicKey, counter, credentialID, attestationObject } = verification.registrationInfo;
-  //   console.log("username", username);
+  if (verification.verified && verification.registrationInfo) {
+    const { credentialPublicKey, counter, credentialID, attestationObject } = verification.registrationInfo;
+    console.log("username", username);
 
-  //   await pool.query('UPDATE profiles SET credentialPublicKey = $1, counter = $2, credentialID = $3, attestationobject = $4 WHERE username = $5',
-  //     [credentialPublicKey, counter, credentialID, attestationObject, username]);
-  // }
+    await pool.query('UPDATE profiles SET credentialPublicKey = $1, counter = $2, credentialID = $3, attestationobject = $4 WHERE username = $5',
+      [credentialPublicKey, counter, credentialID, attestationObject, username]);
+  }
 
   res.status(200).json({});
 }
